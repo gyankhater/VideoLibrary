5f6247b7db58de45fa0b84d43cce27b24314a17a
gyankhater <gyankhater@yahoo.co.in>
2019-08-23 20:31:43 +0530

    jquery data table added for customer

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using Vidly.Models;
 using Vidly.Dtos;
 using AutoMapper;
+using System.Data.Entity;
+
 
 namespace Vidly.Controllers.Api
 {
@@ -21,9 +23,14 @@ namespace Vidly.Controllers.Api
 			}
 
 			//GET /api/customers
-			public IEnumerable<CustomerDto> GetCustomers()
+			public IHttpActionResult GetCustomers()
 			{
-				return _context.Customers.ToList().Select(Mapper.Map<Customer,CustomerDto>);
+				var customerDtos = _context.Customers
+					.Include(c=> c.MembershipType)
+					.ToList()
+					.Select(Mapper.Map<Customer, CustomerDto>);
+
+				return Ok(customerDtos);
 			}
 
 
